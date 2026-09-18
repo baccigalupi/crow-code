e7492366d3836aaf6ff4a6df7877d34fcaff3872
Kane Baccigalupi <baccigalupi@gmail.com>
2026-09-18 12:39:28 -0700

    Move ExtractModelResponse into src/plan with generic response types

---
+++ src/plan/extract-model-response.ts
@@ -0,0 +1,38 @@
+type Message = { content?: string }
+type Choice = { message?: Message }
+type ModelResponse = { choices?: Choice[] }
+
+export class ExtractModelResponse<T> {
+  private response: Response
+  private parse: (content: string) => T
+  private content: string
+
+  constructor(response: Response, parse: (content: string) => T) {
+    this.response = response
+    this.parse = parse
+    this.content = ''
+  }
+
+  async extract() {
+    if (this.response.ok && this.extractContent(await this.response.json())) {
+      return this.parse(this.content)
+    } else {
+      return this.parse('')
+    }
+  }
+
+  private extractContent(body: ModelResponse) {
+    if (body.choices === undefined || body.choices.length === 0) {
+      return false
+    }
+    return this.extractMessage(body.choices[0].message)
+  }
+
+  private extractMessage(message: Message | undefined) {
+    if (message === undefined || message.content === undefined) {
+      return false
+    }
+    this.content = message.content
+    return true
+  }
+}
--- src/plan/goals/model-response-parser.ts
+++ src/plan/goals/model-response-parser.ts
@@ -1,43 +1,5 @@
 import { parseGoals } from './parse-goals.ts'
-
-type GoalMessage = { content?: string }
-type GoalChoice = { message?: GoalMessage }
-type ModelResponse = { choices?: GoalChoice[] }
-
-class ExtractModelResponse<T> {
-  private response: Response
-  private parse: (content: string) => T
-  private content: string
-
-  constructor(response: Response, parse: (content: string) => T) {
-    this.response = response
-    this.parse = parse
-    this.content = ''
-  }
-
-  async extract() {
-    if (this.response.ok && this.extractContent(await this.response.json())) {
-      return this.parse(this.content)
-    } else {
-      return this.parse('')
-    }
-  }
-
-  private extractContent(body: ModelResponse) {
-    if (body.choices === undefined || body.choices.length === 0) {
-      return false
-    }
-    return this.extractMessage(body.choices[0].message)
-  }
-
-  private extractMessage(message: GoalMessage | undefined) {
-    if (message === undefined || message.content === undefined) {
-      return false
-    }
-    this.content = message.content
-    return true
-  }
-}
+import { ExtractModelResponse } from '../extract-model-response.ts'
 
 export const parseModelResponse = (response: Response) => {
   return new ExtractModelResponse(response, parseGoals).extract()
